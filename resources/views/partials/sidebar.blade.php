e2ce5eadc14761d7f324ce5305357dc9e0857113
Tua24 <elyannitua@gmail.com>
2026-01-11 18:17:49 -0400

    Ajuste de sidebar

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -94,6 +94,18 @@
                 <span class="nav-link-text">Proveedores</span>
             </a>
         </li>
+        {{-- NUEVO: Roles y Permisos --}}
+            @can('view roles')
+            <li class="nav-item">
+                <a class="nav-link {{ request()->is('dashboard/roles*') ? 'active' : '' }}" 
+                   href="{{ route('dashboard.roles.index') }}">
+                    <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
+                        <i class="material-icons opacity-10">admin_panel_settings</i>
+                    </div>
+                    <span class="nav-link-text ms-1">Roles y Permisos</span>
+                </a>
+            </li>
+            @endcan
     </ul>
 
     <!-- SECCIÓN 3: NUTRICIÓN ANIMAL -->
